f9277cf65e51b8d33976dabd412fcf237f2d5cde
warc3l <marcel.vilalta@gmail.com>
2022-07-17 19:55:16 +0200

    Include missing files from previous commit #8

--- gui/subclasses/OwlyRendered.cpp
+++ gui/subclasses/OwlyRendered.cpp
@@ -2,4 +2,19 @@
 // Created by Marcel Vilalta i Soler on 14/7/22.
 //
 
+#include <iostream>
 #include "OwlyRendered.h"
+/*
+
+void OwlyRendered::initializeGL() {
+
+
+}
+
+void OwlyRendered::resizeGL(int w, int h) {
+
+}
+
+void OwlyRendered::paintGL() {
+}
+*/
--- gui/subclasses/OwlyRendered.h
+++ gui/subclasses/OwlyRendered.h
@@ -6,11 +6,32 @@
 #define OWLIES_OWLYRENDERED_H
 
 
+#include <QOpenGLWidget>
+// #include <GL/glew.h>
 
-class OwlyRendered {
+class OwlyRendered : public QOpenGLWidget {
 
+public:
+    explicit OwlyRendered(QWidget* parent = nullptr) : QOpenGLWidget(parent) {
+
+    };
+
+    ~OwlyRendered() {
+
+    }
+
+protected:
+    /*
+    void initializeGL();
+
+    void resizeGL(int w, int h);
+
+
+protected slots:
+    void paintGL();
+
+    */
 };
-
 
 
 #endif //OWLIES_OWLYRENDERED_H
